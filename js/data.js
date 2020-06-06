10e2f425c5a6e5681b3a474632f1dbac509abbd7
Yehyoung Kang <pastelmind@kaist.ac.kr>
2020-06-06 23:01:26 +0900

    test: Add sample guide data

---
+++ js/data.js
@@ -0,0 +1,95 @@
+// Sample data
+
+const guides = [
+  {
+    title: "Totally awesome Teemo guide",
+    tier: "gold",
+    author: "tom",
+    createdAt: "2020-06-06T13:39:41.382Z",
+    upvotes: 100,
+    downvotes: 50,
+    views: 150,
+    content: [
+      {
+        type: "empty",
+        summary: "Step 1. Learn how to use the E skill",
+      },
+      {
+        type: "text",
+        summary: "Step 2. Use Teemo's long attack range to poke",
+        content:
+          "There are two ways of consuming the opponent's life.\n\n1. When your opponent wants to kill minions, attack him so that he cannot resist soon.\n Buy shoes ASAP to increase your speed, so that you can escape",
+      },
+      {
+        type: "video",
+        summary: "Plant mushrooms along the wall to prevent ganks",
+        videoUrl: "https://www.youtube.com/watch?v=UVmgwL3tOAg",
+      },
+    ],
+  },
+  {
+    title: "Items must but ASAP for Teemo",
+    tier: "diamond",
+    author: "jack",
+    createdAt: "2020-05-12T14:20:21.382Z",
+    upvotes: 10000,
+    downvotes: 2350,
+    views: 1121150,
+    content: [
+      {
+        type: "empty",
+        summary: "Step 1. Buy long sword as your first small item",
+      },
+      {
+        type: "empty",
+        summary: "Step 2. Buy the hourglass as your first complete item",
+      },
+      {
+        type: "video",
+        summary: "Plant mushrooms along the wall to prevent ganks",
+        videoUrl: "https://www.youtube.com/watch?v=UVmgwL3tOAg",
+      },
+    ],
+  },
+  {
+    title: "Guides by SKT Faker",
+    tier: "master",
+    author: "Faker_Forever",
+    createdAt: "2020-05-01T04:25:25.382Z",
+    upvotes: 9433,
+    downvotes: 251,
+    views: 12493,
+    content: [
+      {
+        type: "video",
+        summary: "HOW TO READ THE MAP by Faker",
+        videoUrl: "https://www.youtube.com/watch?v=-iD8Ab05N1s",
+      },
+      {
+        type: "video",
+        summary: "RYZE GUIDE by Faker",
+        videoUrl: "https://www.youtube.com/watch?v=Pd5dp2f_yUg",
+      },
+      {
+        type: "video",
+        summary: "Unique ZED GUIDE by Faker",
+        videoUrl: "https://www.youtube.com/watch?v=mvGVELBadT8",
+      },
+      {
+        type: "video",
+        summary: "Faker's HOW TO ZOE",
+        videoUrl: "https://www.youtube.com/watch?v=18kgAyq0Rxw",
+      },
+    ],
+  },
+  {
+    title: "Empty guide (for testing)",
+    tier: "bronze",
+    author: "SomeUser123",
+    createdAt: "2020-05-15T06:21:45.448Z",
+    upvotes: 12,
+    downvotes: 124,
+    views: 178,
+    content: [],
+  },
+];
